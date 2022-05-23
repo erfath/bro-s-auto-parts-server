b9400a411595716f59bd7bea73180dc3d55934bc
Erfath <erfathparvez1@gmail.com.com>
2022-05-24 04:17:15 +0600

    got all item

--- index.js
+++ index.js
@@ -13,21 +13,33 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.28yeg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-console.log('connected')
+        const itemCollection = client.db('auto-parts').collection('items');
+
+        app.get('/item', async (req, res)=>{
+            const query = {}
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+
+
+
+
+
     }
 
-    finally{}
+    finally { }
 }
 
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hey, Can You Hear Me?')
+    res.send('Hey, Can You Hear Me?')
 })
 
 app.listen(port, () => {
-  console.log(`Car parts owner is listening on port ${port}`)
+    console.log(`Car parts owner is listening on port ${port}`)
 })
