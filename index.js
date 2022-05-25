f98d9e6bf463ec0b8ff5d86387fe43b285283db8
Erfath <erfathparvez1@gmail.com.com>
2022-05-25 21:41:29 +0600

    JWT added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -13,6 +14,21 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.28yeg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbiden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         await client.connect();
@@ -20,7 +36,7 @@ async function run() {
         const orderCollection = client.db('auto-parts').collection('orders');
         const userCollection = client.db('auto-parts').collection('users');
 
-        app.get('/item', async (req, res)=>{
+        app.get('/item', async (req, res) => {
             const query = {}
             const cursor = itemCollection.find(query);
             const items = await cursor.toArray();
@@ -33,30 +49,55 @@ async function run() {
             const item = await itemCollection.findOne(query)
             res.send(item);
         });
-        app.put('/user/:email', async (req,  res)=>{
+
+        app.get('/user', verifyJWT, async (req, res)=>{
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const user = req.body;            
-            const filter = {email: email};
-            const options = {upsert : true};
+            const filter = { email: email };
             const updateDoc = {
-                $set : user,
+                $set: {role:'admin'},
             };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const result = await userCollection.updateOne(filter, updateDoc);    
             res.send(result);
 
         })
 
-        app.post('/order', async (req, res)=>{
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token });
+
+        })
+
+        app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
-        app.get('/order', async (req, res)=>{
+        app.get('/order', verifyJWT, async (req, res) => {
             const buyerEmail = req.query.buyerEmail;
-            const query = {buyerEmail : buyerEmail};
-            const orders = await orderCollection.find(query).toArray();
-            res.send(orders);
+            const decodedEmail = req.decoded.email;
+            if (buyerEmail === decodedEmail) {
+                const query = { buyerEmail: buyerEmail };
+                const orders = await orderCollection.find(query).toArray();
+                return res.send(orders);
+            }
+            else{
+                return res.status(403).send({ message: 'Forbiden Access' })
+            }
+
         })
 
 
